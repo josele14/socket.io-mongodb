bf03645c652d57eb68ff10079ffb9236b887f9bb
josele14 <jmoleroguerra@gmail.com>
2021-05-25 01:55:12 +0200

    servidor

---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", socket => {
+    console.log("New user connected");
+    socket.on("disconnect", () => {
+        console.log("A user has disconnected")
+    });
+    socket.on("chat message", msg => {
+        console.log("message: " + msg);
+        io.emit("chat message", msg);
+    });
+});
+
+http.listen(port, ()=> {
+    console.log(`Listening on ${port}`);
+});
