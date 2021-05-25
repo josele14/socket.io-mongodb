26d6cd88f6f029b495d2dc0162a7a321a6073c23
josele14 <jmoleroguerra@gmail.com>
2021-05-25 04:56:45 +0200

    Utilizar pug templating (Completa el commit previo)

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+app.set('view engine', 'pug');
 let port = process.env.PORT || 3000;
 
+const users = new Array();
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    // res.sendFile(__dirname + "/index.html");
+    res.render("index", {});
+});
+
+app.get("/chat", (req, res) => {
+    users.push({"username": req.query.username});  
+    // res.sendFile(__dirname + "/chat.html");
+    res.render("chat", {"username": req.query.username});
 });
 
 io.on("connection", socket => {
