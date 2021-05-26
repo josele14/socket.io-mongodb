4e20c6e714be4cb9102262ad77af4d49cbeef1f8
josele14 <jmoleroguerra@gmail.com>
2021-05-26 03:29:22 +0200

    ordenar server e importar BD class

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const DefcommDB = require('DefcommDB');
 app.set('view engine', 'pug');
 let port = process.env.PORT || 3000;
 
-const users = new Array();
+const db = new DefcommDB();
+
+console.log(db);
 
 app.get("/", (req, res) => {
-    // res.sendFile(__dirname + "/index.html");
     res.render("index", {});
 });
 
 app.get("/chat", (req, res) => {
-    users.push({ "username": req.query.username });
-    // res.sendFile(__dirname + "/chat.html");
+    // users.push({ "username": req.query.username });
     res.render("chat", { username: req.query.username });
 });
 
 io.on("connection", socket => {
-    // const sessionID = socket.id;
-    // console.log(`New user ${sessionID} connected`);
+    // New user connects
     socket.on("New user", user => {
         console.log(user + " has connected");
         let msg = user + " ENTERS INTO THE ROOM";
         io.emit("srv message", {user: '', msg: msg});
     });
+    // Disconnects
     socket.on("disconnect", () => {
         console.log("A user has disconnected");
     });
+    // User sent a message
     socket.on("user message", data => {
         io.emit("srv message", {user: data.user, msg: data.msg });
     });
 });
-
-
 
 http.listen(port, () => {
     console.log(`Listening on ${port}`);
